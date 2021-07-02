4b6d51d6cba4b2b24477a1731cd734b3220a3af4
kari-36 <83143204+kari-36@users.noreply.github.com>
2021-07-02 17:50:14 +0500

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1601,14 +1601,14 @@ async def cb_about(bot, update: CallbackQuery):
     text=f"<i><u>Bot's Status</u></i>\n"
     text+=f"\n<b><i>Bot's Uptime:</i></b> <code>{time_formatter(time.time() - start_uptime)}</code>\n"
     text+=f"\n<b><i>Bot Funtion:</i></b> <i>Auto Filter Files</i>\n"
-    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/eagleunionelizasupport">@Eagleunionelizasupport</a>\n"""
+    text+=f"""\n<b><i>Bot Support:</i></b> <a href="https://t.me/Black_Knights_Union_Support"@Support</a>\n"""
     text+="""\n<b><i>Source Code:</i></b> <a href="https://github.com/CrazyBojahshtsz/Adv-Filter-Bot-Vsjshhshdhdhdh have2">Source</a>"""
 
     buttons = [
         [
             InlineKeyboardButton
                 (
-                    "My Dev ⚡", url="https://t.me/Slient_boy"
+                    "Support ⚡", url="https://t.me/Black_Knights_Union_Support"
                 ),
                 
             InlineKeyboardButton
